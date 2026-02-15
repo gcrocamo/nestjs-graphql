fd204dfa5697ba38b630cc563767e731bbba1898
gcrocamo <crocamoguilherme@gmail.com>
2026-02-15 12:39:35 -0300

    refactor: update ESLint configuration and remove unused entity files

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,16 +1,15 @@
-import * as path from 'path';
-import { registerAs } from '@nestjs/config';
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import * as path from 'path'
+import { registerAs } from '@nestjs/config'
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions'
 
 export default registerAs(
   'dbconfig.dev',
   (): SqliteConnectionOptions => ({
-    // Don't put this here, Instead put in the env file
     database: process.env.DATABASE_URL,
     type: 'sqlite',
 
     entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
 
     synchronize: true,
-  }),
-);
+  })
+)
